dc59432b9adf092fc9484985ad1d575b428759fc
Aman K. Azad <amanazad38@gmail.com>
2017-07-20 08:29:29 +0530

    Update Linked_List.cpp

--- Linked_List.cpp
+++ Linked_List.cpp
@@ -7,27 +7,34 @@ struct node {
 };
 
 struct node* head;
-void insert( int x)
+void insert( int x, int pos)
 {
-	node* temp =new node();
-	temp -> data = x;
-	temp -> next = NULL;
-	if(head != NULL)
+	
+	node* ele = new node();
+	node* temp = new node();  // for iteration
+	ele -> data = x;
+	ele -> next = NULL;
+	if(n==1)
 	{
-		temp-> next = head;
+	       temp -> next = head;
+	       head = temp;
 	}
-	head = temp;
+	
+	for(int i=0;i<pos-2;i++)
+		temp = temp -> next;
+	ele -> next = temp -> next;
+	temp -> next = ele;
 }
 void print()
 {
-    node* temp = head;
+	node* temp = head;
 	cout<< "List is: ";
 	while (temp != NULL)
-	{
-       cout<< temp -> data <<" ";
-       temp = temp -> next;
-	}
-	cout << "\n" ;
+	  {
+	   cout<< temp -> data <<" ";
+	   temp = temp -> next;
+	  }
+	 cout << "\n" ;
 }
 int main()
 {
